0da2ab08ff80de111da542595f0ab2c6eced3eaa
Timothy Sukamtoh <timtoh168@gmail.com>
2023-06-16 11:17:19 -0700

    bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+"use strict";
+
+/**Sorting algorithm using bubble sort */
+//[4, 20, 12, 10, 7, 9]
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i ; j++) {
+      console.log("i:", i, "j:", j)
+      console.log(arr[j], arr[j+1])
+      if(arr[j] > arr[j+1]){
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]; //swaps places
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
